e13bab7d5e4ca80871e183385eab5cf0e0cef5ca
Presum0 <preethamnelaturi@gmail.com>
2025-10-11 19:01:15 +0530

    components/Navbar

--- NOVA/src/App.jsx
+++ NOVA/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ParticlesBackground from './components/ParticlesBackground';
 import Home from './pages/Home';
